a403dcdd5f68009ede645a30608d9f0b48c84ed1
LuisPiro <lpiroc@gmail.com>
2024-08-24 20:29:03 -0400

    10mo commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require('dotenv').config(); // Asegúrate de que esta línea esté al principio del archivo
+
 const express = require('express');
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
 
-// Serve static files
+const app = express();
+const port = process.env.PORT || 3000; // Usa la variable PORT del archivo .env
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
